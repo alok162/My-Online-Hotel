dae50f9427568dd3d3d010a0e8655a6fe61bf112
Alok <alok@Aloks-MacBook-Pro.local>
2018-04-10 13:52:29 +0530

    added routing functionality

--- django-react/src/routes.js
+++ django-react/src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Router, Route } from 'react-router-dom';
 import Login from './components/Login/Login';
-
+import Body from './components/Body/Body';
 
 export const Routes = () => (
 <BrowserRouter>
   <Switch>
       <Route path='/login' component={ Login }/>
+      <Route path='/' component={ Body }/> 
   </Switch>
 </BrowserRouter>
 );
