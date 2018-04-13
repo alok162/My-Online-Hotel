af962026065fc1143978043a8607726236ed9e01
Alok <alok@Aloks-MacBook-Pro.local>
2018-04-13 17:44:44 +0530

    add urls to access token and refresh token

--- my_online_hotel/urls.py
+++ my_online_hotel/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('myapp.urls'))
+    url(r'^', include('myapp.urls')),
+    url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
 ]
